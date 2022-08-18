a0ebf393bd72c1ae4bea216b7a3f96310e06c40d
Derek Griffin <griffinderek6@gmail.com>
2022-08-18 13:14:01 -0600

    finish

---
+++ ClassesEx1/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace ClassesEx1
+{
+    public class Car
+    {
+        public Car()
+        {
+        }
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public string Year { get; set; }
+    }
+}
+
---
+++ ClassesEx1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ClassesEx1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.Make = "Ford";
+            myCar.Model = "F-150";
+            myCar.Year = "2017";
+
+            Console.WriteLine($"{myCar.Year} {myCar.Make} {myCar.Model}");
+        }
+    }
+}
+
